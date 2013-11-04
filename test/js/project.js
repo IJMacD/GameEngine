4c099feba5812b8869644b56a829af2d5f4a4cd4
Iain MacDonald <IJMacD@gmail.com>
2013-11-04 15:08:07 +0800

    Tabs to spaces

--- test/js/project.js
+++ test/js/project.js
@@ -1,32 +1,32 @@
 $(function() {
-	var GameObject = GE.GameObject,
-		GameComponent = GE.GameComponent,
-		GEC = GE.Comp,
+    var GameObject = GE.GameObject,
+        GameComponent = GE.GameComponent,
+        GEC = GE.Comp,
 
-		canvas = $('#surface'),
-		context = canvas[0].getContext("experimental-webgl"),
-		gl = context,
-		canvasWidth = canvas.width(),
-		canvasHeight = canvas.height(),
-		gameRoot = new GE.GameObjectManager(),
-		cameraSystem,
-		renderSystem,
-		redBall,
-		sun,
-		lastTime = 0;
+        canvas = $('#surface'),
+        context = canvas[0].getContext("experimental-webgl"),
+        gl = context,
+        canvasWidth = canvas.width(),
+        canvasHeight = canvas.height(),
+        gameRoot = new GE.GameObjectManager(),
+        cameraSystem,
+        renderSystem,
+        redBall,
+        sun,
+        lastTime = 0;
 
-	function initCanvas(width,height){
-		// canvas.removeAttr("width");
-		// canvas.removeAttr("height");
-		// canvasWidth = width||canvas.width();
-		// canvasHeight = height||canvas.height();
-		canvas[0].width = canvasWidth;
-		canvas[0].height = canvasHeight;
-		gl.viewportWidth = canvasWidth;
-		gl.viewportHeight = canvasHeight,
-		cameraSystem && cameraSystem.setScreenSize(canvasWidth, canvasHeight);
-		renderSystem && renderSystem.setCanvasSize(canvasWidth, canvasHeight);
-	}
+    function initCanvas(width,height){
+        // canvas.removeAttr("width");
+        // canvas.removeAttr("height");
+        // canvasWidth = width||canvas.width();
+        // canvasHeight = height||canvas.height();
+        canvas[0].width = canvasWidth;
+        canvas[0].height = canvasHeight;
+        gl.viewportWidth = canvasWidth;
+        gl.viewportHeight = canvasHeight,
+        cameraSystem && cameraSystem.setScreenSize(canvasWidth, canvasHeight);
+        renderSystem && renderSystem.setCanvasSize(canvasWidth, canvasHeight);
+    }
 
     function getShader(gl, id) {
         var shaderScript = document.getElementById(id);
@@ -96,32 +96,32 @@ $(function() {
     }
 
 
-	initCanvas();
-	
+    initCanvas();
+    
     initShaders();
 
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
 
-	$('#fullscr-btn').on("click", function(){
-		canvas[0].webkitRequestFullscreen();
-		canvasWidth = window.innerWidth;
-		canvasHeight = window.innerHeight;
-		initCanvas();
-	});
+    $('#fullscr-btn').on("click", function(){
+        canvas[0].webkitRequestFullscreen();
+        canvasWidth = window.innerWidth;
+        canvasHeight = window.innerHeight;
+        initCanvas();
+    });
 
-	$(window).on("resize", function(){
-		canvasWidth = canvas.width();
-		canvasHeight = canvas.height();
-		initCanvas();
-	});
+    $(window).on("resize", function(){
+        canvasWidth = canvas.width();
+        canvasHeight = canvas.height();
+        initCanvas();
+    });
 
-	GE.DEBUG = true;
+    GE.DEBUG = true;
 
-	function RedTriangleRenderingComponent(renderSystem){
-		this.renderSystem = renderSystem;
-		this.buffer = gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+    function RedTriangleRenderingComponent(renderSystem){
+        this.renderSystem = renderSystem;
+        this.buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
         var vertices = [
              0.0,  1.0,  0.0,
             -1.0, -1.0,  0.0,
@@ -130,20 +130,20 @@ $(function() {
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
         this.buffer.itemSize = 3;
         this.buffer.numItems = 3;
-	}
-	RedTriangleRenderingComponent.prototype = new GameComponent();
-	RedTriangleRenderingComponent.prototype.update = function(parent, delta) {
-		var buff = this.buffer;
-		this.renderSystem.push(function(gl,mvMatrix){
-	        mat4.translate(mvMatrix, mvMatrix, [parent.position.x, parent.position.y, -7.0]);
-	        gl.bindBuffer(gl.ARRAY_BUFFER, buff);
-	        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, buff.itemSize, gl.FLOAT, false, 0, 0);
-	        setMatrixUniforms();
-	        gl.drawArrays(gl.TRIANGLES, 0, buff.numItems);
-		});
-	};
-	function RedBoxRenderingComponent(renderSystem){
-		this.renderSystem = renderSystem;
+    }
+    RedTriangleRenderingComponent.prototype = new GameComponent();
+    RedTriangleRenderingComponent.prototype.update = function(parent, delta) {
+        var buff = this.buffer;
+        this.renderSystem.push(function(gl,mvMatrix){
+            mat4.translate(mvMatrix, mvMatrix, [parent.position.x, parent.position.y, -7.0]);
+            gl.bindBuffer(gl.ARRAY_BUFFER, buff);
+            gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, buff.itemSize, gl.FLOAT, false, 0, 0);
+            setMatrixUniforms();
+            gl.drawArrays(gl.TRIANGLES, 0, buff.numItems);
+        });
+    };
+    function RedBoxRenderingComponent(renderSystem){
+        this.renderSystem = renderSystem;
         this.buffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
         vertices = [
@@ -155,63 +155,63 @@ $(function() {
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
         this.buffer.itemSize = 3;
         this.buffer.numItems = 4;
-	}
-	RedBoxRenderingComponent.prototype = new GameComponent();
-	RedBoxRenderingComponent.prototype.update = function(parent, delta) {
-		var buff = this.buffer;
-		this.renderSystem.push(function(gl,mvMatrix){
-	        mat4.translate(mvMatrix, mvMatrix, [parent.position.x, parent.position.y, -7.0]);
-	        gl.bindBuffer(gl.ARRAY_BUFFER, buff);
-	        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, buff.itemSize, gl.FLOAT, false, 0, 0);
-	        setMatrixUniforms();
-	        gl.drawArrays(gl.TRIANGLE_STRIP, 0, buff.numItems);
-		});
-	};
+    }
+    RedBoxRenderingComponent.prototype = new GameComponent();
+    RedBoxRenderingComponent.prototype.update = function(parent, delta) {
+        var buff = this.buffer;
+        this.renderSystem.push(function(gl,mvMatrix){
+            mat4.translate(mvMatrix, mvMatrix, [parent.position.x, parent.position.y, -7.0]);
+            gl.bindBuffer(gl.ARRAY_BUFFER, buff);
+            gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, buff.itemSize, gl.FLOAT, false, 0, 0);
+            setMatrixUniforms();
+            gl.drawArrays(gl.TRIANGLE_STRIP, 0, buff.numItems);
+        });
+    };
 
-	cameraSystem = new GE.CameraSystem(0, 0, canvasWidth, canvasHeight);
-	renderSystem = new GE.WebGLRenderSystem(context, canvasWidth, canvasHeight, cameraSystem, mvMatrix, pMatrix);
-	cameraSystem.setScale(0.5);
+    cameraSystem = new GE.CameraSystem(0, 0, canvasWidth, canvasHeight);
+    renderSystem = new GE.WebGLRenderSystem(context, canvasWidth, canvasHeight, cameraSystem, mvMatrix, pMatrix);
+    cameraSystem.setScale(0.5);
 
-	// cameraSystem.addComponent(new GEC.RotationComponent(0.0003));
-
-
-	sun = new GameObject();
-	sun.mass = 0.1;
-	sun.setPosition(0,0);
-	//sun.addComponent({update:function(p){renderSystem.push(function(c){c.fillStyle="black";c.beginPath();c.arc(p.position.x,p.position.y,2,0,Math.PI*2);c.fill();})}});
-	sun.addComponent(new RedBoxRenderingComponent(renderSystem));
-
-	gameRoot.addObject(sun);
-
-	for(var i = 0; i < 10; i++){
-		redBall = new GameObject();
-		redBall.setPosition(Math.random()*200-100,Math.random()*200-100);
-		redBall.setVelocity(Math.random()*0.2-0.1,Math.random()*0.2-0.1);
-
-		redBall.addComponent(new GEC.MoveComponent());
-		redBall.addComponent(new GEC.PointGravityComponent(sun));
-		redBall.addComponent(new GEC.RotationComponent(Math.random()*0.002 - 0.001));
-		var r = Math.random();
-		if(r < 0.5) {
-			redBall.addComponent(new RedTriangleRenderingComponent(renderSystem));
-		}
-		else {
-			redBall.addComponent(new RedBoxRenderingComponent(renderSystem));
-		}
-
-		gameRoot.addObject(redBall);
-	}
+    // cameraSystem.addComponent(new GEC.RotationComponent(0.0003));
 
 
-	gameRoot.addObject(cameraSystem);
-	gameRoot.addObject(renderSystem);
+    sun = new GameObject();
+    sun.mass = 0.1;
+    sun.setPosition(0,0);
+    //sun.addComponent({update:function(p){renderSystem.push(function(c){c.fillStyle="black";c.beginPath();c.arc(p.position.x,p.position.y,2,0,Math.PI*2);c.fill();})}});
+    sun.addComponent(new RedBoxRenderingComponent(renderSystem));
+
+    gameRoot.addObject(sun);
+
+    for(var i = 0; i < 10; i++){
+        redBall = new GameObject();
+        redBall.setPosition(Math.random()*200-100,Math.random()*200-100);
+        redBall.setVelocity(Math.random()*0.2-0.1,Math.random()*0.2-0.1);
+
+        redBall.addComponent(new GEC.MoveComponent());
+        redBall.addComponent(new GEC.PointGravityComponent(sun));
+        redBall.addComponent(new GEC.RotationComponent(Math.random()*0.002 - 0.001));
+        var r = Math.random();
+        if(r < 0.5) {
+            redBall.addComponent(new RedTriangleRenderingComponent(renderSystem));
+        }
+        else {
+            redBall.addComponent(new RedBoxRenderingComponent(renderSystem));
+        }
+
+        gameRoot.addObject(redBall);
+    }
 
 
-	function loop(time){
-		requestAnimationFrame(loop);
-		gameRoot.update(Math.min(time - lastTime,100));
-		lastTime = time;
-	}
-	loop(0);
+    gameRoot.addObject(cameraSystem);
+    gameRoot.addObject(renderSystem);
+
+
+    function loop(time){
+        requestAnimationFrame(loop);
+        gameRoot.update(Math.min(time - lastTime,100));
+        lastTime = time;
+    }
+    loop(0);
 
 });
