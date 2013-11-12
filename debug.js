7e00e776d2f1f0ea0abffc8b0ef161a66b95ec0f
Iain MacDonald <IJMacD@gmail.com>
2013-11-12 18:06:20 +0800

    Tidy up debug renderer

--- debug.js
+++ debug.js
@@ -22,39 +22,50 @@ var GE = (function(GE){
 	DebugDrawPathComponent.prototype = new GameComponent();
 	DebugDrawPathComponent.prototype.update = function(parent, delta) {
 		if(GE.DEBUG){
+			var px = parent.position[0],
+				py = parent.position[1],
+				vx = parent.velocity[0],
+				vy = parent.velocity[1],
+				ax = (vx - this.lastVx)/delta,
+				ay = (vy - this.lastVy)/delta,
+				rx = this.relativeTo[0],
+				ry = this.relativeTo[1],
+				skip = this.pathIndex % this.pathSize,
+				path = [px, py];
+
 			// Draw Path
-			var skip = this.pathIndex % this.pathSize,
-				path = [parent.position[0], parent.position[1]];
 			if(this.pathIndex > this.pathSize){
 				for(var i = this.pathSize-1;i>=0;i--){
 					var index = (i + skip + this.pathSize) % this.pathSize;
-					path.push(this.path[index][0]+this.relativeTo[0],this.path[index][1]+this.relativeTo[1]);
+					path.push(
+						this.path[index][0]+rx,
+						this.path[index][1]+ry
+					);
 				}
 			}else{
 				for(var i = this.pathIndex-1;i>=0;i--){
-					path.push(this.path[i][0]+this.relativeTo[0],this.path[i][1]+this.relativeTo[1]);
+					path.push(
+						this.path[i][0]+rx,
+						this.path[i][1]+ry
+					);
 				}
 			}
-			if(this.relativeTo[0])
+
+			if(rx || ry)
 				this.renderSystem.strokePath(path,"#CCF",0);
 			else
 				this.renderSystem.strokePath(path,"#CCC",0);
+
 			this.pathIndex++;
-			this.path[skip] = [parent.position[0]-this.relativeTo[0],parent.position[1]-this.relativeTo[1]];
+			this.path[skip] = [px-rx,py-ry];
 
 			// Draw Velocity
-			this.renderSystem.strokePath([parent.position[0], parent.position[1],
-				parent.position[0]+parent.velocity[0]*100, parent.position[1]+parent.velocity[1]*100],
-				"rgba(0,128,255,0.7)",0);
+			this.renderSystem.strokePath([px, py, px+vx*100, py+vy*100], "rgba(0,128,255,0.7)",0);
 
 			// Draw Acceleration
-			var ax = (parent.velocity[0] - this.lastVx)/delta,
-				ay = (parent.velocity[1] - this.lastVy)/delta;
-			this.lastVx = parent.velocity[0];
-			this.lastVy = parent.velocity[1];
-			this.renderSystem.strokePath([parent.position[0], parent.position[1],
-				parent.position.x+ax*4e5, parent.position[1]+ay*4e5],
-				"rgba(0,255,0,0.7)",0);
+			this.renderSystem.strokePath([px, py, px+ax*4e5, py+ay*4e5], "rgba(0,255,0,0.7)",0);
+			this.lastVx = vx;
+			this.lastVy = vy;
 		}else{
 			this.pathIndex = 0;
 		}
