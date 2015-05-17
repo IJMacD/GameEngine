f36d89be35fd272922a7a4cdc2f4a1458965879e
Iain MacDonald <IJMacD@gmail.com>
2015-05-17 22:47:09 +0800

    Added worldSystem

--- world.js
+++ world.js
@@ -2,20 +2,29 @@ var GE = (function(GE){
 
 	GE.Comp = GE.Comp || {};
 
-	var WorldBounceComponent = GE.Comp.WorldBounceComponent = function WorldBounceComponent (width, height, bounds) {
+	function WorldSystem(bounds){
+		this.bounds = bounds;
+	}
+	GE.WorldSystem = WorldSystem;
+	WorldSystem.prototype = new GE.GameObject();
+
+	var WorldBounceComponent = GE.Comp.WorldBounceComponent = function WorldBounceComponent (worldSystem, width, height) {
+		this.worldSystem = worldSystem;
 		this.ax = width / 2;
 		this.ay = height / 2;
-		this.bx1 = bounds[0] + this.ax;
-		this.by1 = bounds[1] + this.ay;
-		this.bx2 = bounds[2] - this.ax;
-		this.by2 = bounds[3] - this.ay;
 	};
 
 	WorldBounceComponent.prototype = new GE.GameComponent();
 
 	WorldBounceComponent.prototype.update = function(parent, delta) {
 		var coef = 0.9,
-			friction = 0.9;
+				friction = 0.9;
+
+		this.bx1 = this.worldSystem.bounds[0] + this.ax;
+		this.by1 = this.worldSystem.bounds[1] + this.ay;
+		this.bx2 = this.worldSystem.bounds[2] - this.ax;
+		this.by2 = this.worldSystem.bounds[3] - this.ay;
+
 		if(parent.position[0] < this.bx1){
 			parent.position[0] = this.bx1;
 			parent.velocity[0] = -parent.velocity[0]*coef;
@@ -38,18 +47,20 @@ var GE = (function(GE){
 		}
 	};
 
-	var WorldWrapComponent = GE.Comp.WorldWrapComponent = function WorldWrapComponent (bounds) {
-		this.ax = bounds[0];
-		this.ay = bounds[1];
-		this.bx = bounds[2];
-		this.by = bounds[3];
-		this.az = bounds[4];
-		this.bz = bounds[5];
+	var WorldWrapComponent = GE.Comp.WorldWrapComponent = function WorldWrapComponent (worldSystem) {
+		this.worldSystem = worldSystem;
 	};
 
 	WorldWrapComponent.prototype = new GE.GameComponent();
 
 	WorldWrapComponent.prototype.update = function(parent, delta) {
+		this.ax = this.worldSystem.bounds[0];
+		this.ay = this.worldSystem.bounds[1];
+		this.bx = this.worldSystem.bounds[2];
+		this.by = this.worldSystem.bounds[3];
+		this.az = this.worldSystem.bounds[4];
+		this.bz = this.worldSystem.bounds[5];
+
 		if(parent.position[0] < this.ax){
 			parent.position[0] = this.bx;
 		}
