e5011adc337e1017b9cb4f8d2ca920bbb8deb3c3
Iain MacDonald <IJMacD@gmail.com>
2016-04-16 14:37:25 +0800

    Template for new modules

---
+++ _blank.js
@@ -0,0 +1,15 @@
+var GE = (function(GE){
+  "use strict";
+
+  GE.Comp = GE.Comp || {};
+
+  var GameObject = GE.GameObject,
+      GameComponent = GE.GameComponent,
+      GEC = GE.Comp;
+
+  /***********************
+   * Module code goes here
+   ***********************/
+
+  return GE;
+}(GE || {}));
