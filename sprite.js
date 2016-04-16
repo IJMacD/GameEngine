20ec0de8e89e7d4b130b45e5dafc30a7983d18ce
Iain MacDonald <IJMacD@gmail.com>
2016-04-16 14:17:21 +0800

    Clarification between textures and sprites

--- sprite.js
+++ sprite.js
@@ -6,50 +6,51 @@ var GE = (function(GE){
       GameComponent = GE.GameComponent,
       GEC = GE.Comp;
 
-  GE.GameComponent.create(function TileComponent(renderSystem, sprite, bounds) {
+  GE.GameComponent.create(function TileComponent(renderSystem, texture, bounds) {
     this.renderSystem = renderSystem;
-    this.sprite = sprite;
+    this.texture = texture;
     this.bounds = bounds;
   }, {
     update: function(parent, delta){
       var renderSystem = this.renderSystem,
-          sprite = this.sprite,
+          texture = this.texture,
           bounds = this.bounds,
-          dx = sprite.width,
-          dy = sprite.height,
+          dx = texture.width,
+          dy = texture.height,
           startX = parent.position[0] % dx,
           startY = parent.position[1] % dy,
           x = bounds[0] + startX - dx,
           y = bounds[1],// + startY - dy,
           width = bounds[2],
           height = bounds[3],
-          render = function(sprite, x, y){
+          render = function(texture, x, y){
             return function(context){
-              context.drawImage(sprite, x, y);
+              context.drawImage(texture.image, x, y);
             };
           };
       for(; y < height + dy; y += dy){
         for(; x < width + dx; x += dx){
-          renderSystem.push(render(sprite, x, y));
+          renderSystem.push(render(texture, x, y));
         }
       }
     }
   });
 
-  GE.GameComponent.create(function SpriteSheetRenderingComponent(renderSystem){
+  GE.GameComponent.create(function SpriteRenderingComponent(renderSystem){
     this.renderSystem = renderSystem;
   }, {
     update: function(parent, delta) {
-      var frame = parent.sprite[parent.spriteIndex%parent.sprite.length],
-          image = frame && frame.i;
+      var sprite = parent.sprite,
+          image = sprite && sprite.t.image;
+
       this.renderSystem.push(function(context){
         var x = parent.position[0],
             y = parent.position[1],
-            w = frame.w,
-            h = frame.h;
+            w = sprite.w,
+            h = sprite.h;
         context.translate(x,y);
         context.rotate(parent.rotation);
-        context.drawImage(image, frame.x, frame.y, w, h, -frame.ox, -frame.oy, w, h);
+        context.drawImage(image, sprite.x, sprite.y, w, h, -sprite.ox, -sprite.oy, w, h);
       });
     }
   });
@@ -57,12 +58,16 @@ var GE = (function(GE){
   GE.GameComponent.create(function SpriteAnimationComponent(duration){
       this.duration = duration;
       this.countdown = duration;
+      this.spriteIndex = 0;
   }, {
     update: function(parent, delta) {
-      var spriteCount = parent.sprite.length;
+      var spriteCount = parent.sprites.length;
+
       this.countdown -= delta;
       if(this.countdown < 0){
-        parent.spriteIndex = (parent.spriteIndex + 1) % spriteCount;
+        // TODO: Possible divide by zero
+        this.spriteIndex = (this.spriteIndex + 1) % spriteCount;
+        parent.sprite = parent.sprites[this.spriteIndex];
         this.countdown = this.duration;
       }
     }
