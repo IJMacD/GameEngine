5994d50f941e665296d7bfbd5a1093923171275b
Iain MacDonald <IJMacD@gmail.com>
2016-08-18 19:12:24 +0800

    Convert to es6 classes

--- core.js
+++ core.js
@@ -1,22 +1,20 @@
 import { vec3 } from 'gl-matrix';
 
-	// Exported at end.
+	export class GameObject {
+    constructor () {
+  		this.components = [];
+  		this.position = vec3.create();
+  		this.velocity = vec3.create();
+  		this.rotation = 0;
+  		this.rotationSpeed = 0;
+  		this.toBeRemoved = [];
+  		this.life = 1;
+  		this.team = 0;
 
-	export function GameObject(){
-		this.components = [];
-		this.position = vec3.create();
-		this.velocity = vec3.create();
-		this.rotation = 0;
-		this.rotationSpeed = 0;
-		this.toBeRemoved = [];
-		this.life = 1;
-		this.team = 0;
+  		this.components.remove = arrayRemoveItem;
+  	}
 
-		this.components.remove = arrayRemoveItem;
-	};
-
-	GameObject.prototype = {
-		addComponent: function(component){
+		addComponent (component){
 
 			// Allow syntactic sugar of addComponent(function() {...}) which is a
 			// shorthand for specifying a simple component with only an update method
@@ -33,25 +31,29 @@ import { vec3 } from 'gl-matrix';
 			}
 
 			return this;
-		},
-		removeComponent: function(component){
+		}
+
+		removeComponent (component){
 			this.toBeRemoved.push(component);
 			return this;
-		},
-		removeComponentByName: function(name){
+		}
+
+		removeComponentByName (name){
 			for(var i = 0; i < this.components.length; i++){
 				if(this.components[i].name == name)
 					this.toBeRemoved.push(this.components[i]);
 			}
 			return this;
-		},
-		removeComponentByTest: function(test){
+		}
+
+		removeComponentByTest (test){
 			for(var i = 0; i < this.components.length; i++){
 				if(test(this.components[i]))
 					this.toBeRemoved.push(this.components[i]);
 			}
 			return this;
-		},
+		}
+
 		/**
 		 * Protective method to set position of the object.
 		 *
@@ -61,33 +63,38 @@ import { vec3 } from 'gl-matrix';
 		 * e.g. `gameObject.setPosition(undefined, 20)` will only set the y
 		 * co-ordinate, leaving x and z at their original values.
 		 */
-		setPosition: function(x,y,z) {
+		setPosition (x,y,z) {
 			if(x == undefined) { x = this.position[0]; }
 			if(y == undefined) { y = this.position[1]; }
 			if(z == undefined) { z = this.position[2]; }
 			vec3.set(this.position, x, y, z);
 			return this;
-		},
-		setVelocity: function(vx,vy,vz) {
+		}
+
+		setVelocity (vx,vy,vz) {
 			if(vx == undefined) { vx = this.velocity[0]; }
 			if(vy == undefined) { vy = this.velocity[1]; }
 			if(vz == undefined) { vz = this.velocity[2]; }
 			vec3.set(this.velocity, vx, vy, vz);
 			return this;
-		},
-		setRotation: function(th) {
+		}
+
+		setRotation (th) {
 			this.rotation = th;
 			return this;
-		},
-		hit: function(victim) {
+		}
+
+		hit (victim) {
 			if(this.hitVictim == null)
 				this.hitVictim = victim;
-		},
-		hitBy: function(attacker) {
+		}
+
+		hitBy (attacker) {
 			if(this.attackerHit == null)
 				this.attackerHit = attacker;
-		},
-		update: function(delta){
+		}
+
+		update (delta){
 			var i = 0,
 				l = this.components.length,
 				j = 0,
@@ -106,8 +113,9 @@ import { vec3 } from 'gl-matrix';
 			for(i=0;i<l;i++){
 				this.components[i].update(this, delta);
 			}
-		},
-		toHTML: function() {
+		}
+
+		toHTML () {
 			var html = this.name,
 				i;
 			if(typeof this.position.x == "number")
@@ -120,95 +128,108 @@ import { vec3 } from 'gl-matrix';
 			}
 			return html;
 		}
-	};
+	}
 
-	export function GameObjectManager (){
-		GameObject.call(this);
+	export class GameObjectManager extends GameObject {
+    constructor() {
+  		super();
 
-		this.objects = [];
-		this.objectsToBeRemoved = [];
+  		this.objects = [];
+  		this.objectsToBeRemoved = [];
 
-		this.objects.remove = arrayRemoveItem;
-	};
+  		this.objects.remove = arrayRemoveItem;
+  	}
 
-	GameObjectManager.prototype = new GameObject();
+  	addObject (object){
+  		if(object instanceof GameObject)
+  			this.objects.push(object);
+  		object.parent = this;
+  		return this;
+  	}
 
+  	addObjectAt (index, object){
+  		if(object instanceof GameObject)
+  			this.objects.splice(index,0,object);
+  		object.parent = this;
+  		return this;
+  	}
 
-	GameObjectManager.prototype.addObject = function(object){
-		if(object instanceof GameObject)
-			this.objects.push(object);
-		object.parent = this;
-		return this;
-	};
-	GameObjectManager.prototype.addObjectAt = function(index, object){
-		if(object instanceof GameObject)
-			this.objects.splice(index,0,object);
-		object.parent = this;
-		return this;
-	};
-	GameObjectManager.prototype.removeObject = function(object){
-		if(object instanceof GameObject)
-			this.objectsToBeRemoved.push(object);
-		if(object.parent == this) { object.parent = null; }
-		return this;
-	};
-	GameObjectManager.prototype.removeAll = function() {
-		this.objects.length = 0;
-	};
-	GameObjectManager.prototype.update = function(delta){
-		GameObject.prototype.update.call(this, delta);
+  	removeObject (object){
+  		if(object instanceof GameObject)
+  			this.objectsToBeRemoved.push(object);
+  		if(object.parent == this) { object.parent = null; }
+  		return this;
+  	}
 
-		var i = 0,
-			l = this.objects.length,
-			m,
-			j = 0;
+  	removeAll () {
+  		this.objects.length = 0;
+  	}
 
-		for(i=0;i<l;i++){
-			if(this.objects[i].life)
-				this.objects[i].update(delta);
-			else
-				this.removeObject(this.objects[i]);
-		}
+  	update (delta) {
+  		GameObject.prototype.update.call(this, delta);
 
-		m = this.objectsToBeRemoved.length;
+  		var i = 0,
+  			l = this.objects.length,
+  			m,
+  			j = 0;
 
-		for(;j<m;j++){
-			i = 0;
-			for(;i<l;i++){
-				if(this.objects[i] == this.objectsToBeRemoved[j]){
-					this.objects.remove(i);
-					l--;
-					break;
-				}
-			}
-		}
-		this.objectsToBeRemoved.length = 0;
-	};
-	GameObjectManager.prototype.toHTML = function() {
-		var html = this.name,
-			i;
-		if(this.objects.length > 1)
-			html += " (" + this.objects.length + " items)";
-		if(this.components.length){
-			html += "<ul>";
-			for(i=0;i<this.components.length;i++)
-				html += "<li>"+this.components[i].toHTML();
-			html += "</ul>";
-		}
-		if(this.objects.length){
-			html += "<ul>";
-			for(i=0;i<this.objects.length;i++)
-				html += "<li>"+this.objects[i].toHTML();
-			html += "</ul>";
-		}
-		return html;
-	};
+  		for(i=0;i<l;i++){
+  			if(this.objects[i].life)
+  				this.objects[i].update(delta);
+  			else
+  				this.removeObject(this.objects[i]);
+  		}
 
-	export function GameComponent(){};
-	GameComponent.prototype.update = function(parent, delta){};
-	GameComponent.prototype.toHTML = function() {
-		return this.name;
-	};
+  		m = this.objectsToBeRemoved.length;
+
+  		for(;j<m;j++){
+  			i = 0;
+  			for(;i<l;i++){
+  				if(this.objects[i] == this.objectsToBeRemoved[j]){
+  					this.objects.remove(i);
+  					l--;
+  					break;
+  				}
+  			}
+  		}
+  		this.objectsToBeRemoved.length = 0;
+  	}
+
+  	toHTML () {
+  		var html = this.name,
+  			i;
+  		if(this.objects.length > 1)
+  			html += " (" + this.objects.length + " items)";
+  		if(this.components.length){
+  			html += "<ul>";
+  			for(i=0;i<this.components.length;i++)
+  				html += "<li>"+this.components[i].toHTML();
+  			html += "</ul>";
+  		}
+  		if(this.objects.length){
+  			html += "<ul>";
+  			for(i=0;i<this.objects.length;i++)
+  				html += "<li>"+this.objects[i].toHTML();
+  			html += "</ul>";
+  		}
+  		return html;
+  	}
+  }
+
+	export class GameComponent {
+    /**
+     * @abstract
+     */
+  	update (parent, delta){}
+
+  	toHTML () {
+  		return this.name;
+  	}
+  }
+
+  /**
+   * @deprecated Use ES6 extends
+   */
 	GameComponent.create = function(constructor, properties){
 		constructor.prototype = new GameComponent();
 		for(var prop in properties){
