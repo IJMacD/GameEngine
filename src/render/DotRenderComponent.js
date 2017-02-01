e8e6f4232fd0d41dc5fa88b492e6c9e635d9838b
Iain MacDonald <IJMacD@gmail.com>
2017-02-01 12:29:44 +0800

    Add DotRenderComponent

---
+++ src/render/DotRenderComponent.js
@@ -0,0 +1,24 @@
+import GameComponent from '../core/GameComponent';
+
+export default class DotRenderComponent extends GameComponent {
+    constructor (renderSystem, color = "#000") {
+        super();
+
+        this.renderSystem = renderSystem;
+        this.color = color;
+    }
+
+    update (parent, delta) {
+        const b = parent.bounds;
+        const p = parent.position;
+
+        this.renderSystem.push(ctx => {
+            ctx.beginPath();
+            ctx.fillStyle = this.color;
+            ctx.translate(p[0], p[1]);
+            if(b) ctx.scale(b[2] - b[0], b[3] - b[1]);
+            ctx.arc(0, 0, 0.5, 0, Math.PI*2, false);
+            ctx.fill();
+        });
+    }
+}
