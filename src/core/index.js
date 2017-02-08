b0f0b1e86a53f568d829e0188ec46d7ca7bee8f7
Iain MacDonald <IJMacD@gmail.com>
2017-02-08 18:35:43 +0800

    Add index to core

---
+++ src/core/index.js
@@ -0,0 +1,14 @@
+import GameObject from './GameObject';
+
+import GameComponent from './GameComponent';
+
+import GameObjectManager from './GameObjectManager';
+
+import Game from './Game';
+
+export {
+    GameObject,
+    GameComponent,
+    GameObjectManager,
+    Game
+};
