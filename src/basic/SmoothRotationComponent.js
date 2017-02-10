c1f1be393f0e9962c0f8210983213cd9f129a3ad
Iain MacDonald <IJMacD@gmail.com>
2017-02-10 17:22:41 +0800

    Alter how SmoothRotationComponent operates.
    Now takes lead from parent and uses own properties as backing rather than targets.
    
    Is this a better way to go?

--- src/basic/SmoothRotationComponent.js
+++ src/basic/SmoothRotationComponent.js
@@ -1,16 +1,19 @@
 import GameComponent from '../core/GameComponent';
 
-export default class RotationInterpolatorComponent extends GameComponent {
+export default class SmoothRotationComponent extends GameComponent {
     update (parent, delta) {
-        var rotation = parent.rotation,
-            target = this.rotation,
-            diff = target - rotation,
-            speed = 0.01;
-        if(diff > Math.PI){
-            diff -= Math.PI * 2;
-        } else if (diff < -Math.PI){
-            diff += Math.PI * 2;
+        if(this.rotation != parent.rotation) {
+            var rotation = this.rotation,
+                target = parent.rotation,
+                diff = target - rotation,
+                speed = 0.01;
+            if(diff > Math.PI){
+                diff -= Math.PI * 2;
+            } else if (diff < -Math.PI){
+                diff += Math.PI * 2;
+            }
+            parent.rotation = rotation + diff * delta * speed;
+            this.rotation = parent.rotation;
         }
-        parent.rotation = rotation + diff * delta * speed;
     }
 }
