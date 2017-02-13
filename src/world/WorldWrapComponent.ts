0b49c911a78bf3e20b105a800286811cfabf6e28
Iain MacDonald <IJMacD@gmail.com>
2017-02-13 21:20:13 +0800

    Wrap based on bounds rather than raw position

--- src/world/WorldWrapComponent.ts
+++ src/world/WorldWrapComponent.ts
@@ -1,4 +1,5 @@
-import GameComponent from '../core/GameComponent.ts';
+import GameObject from '../core/GameObject';
+import GameComponent from '../core/GameComponent';
 import WorldSystem from './WorldSystem';
 
 /**
@@ -15,45 +16,57 @@ class WorldWrapComponent extends GameComponent {
 		this.worldSystem = worldSystem;
 	}
 
-	update (parent, delta) {
-		const ax = this.worldSystem.bounds[0];
-		const ay = this.worldSystem.bounds[1];
-		const bx = this.worldSystem.bounds[2];
-		const by = this.worldSystem.bounds[3];
-		const az = this.worldSystem.bounds[4];
-		const bz = this.worldSystem.bounds[5];
+	update (parent: GameObject, delta: number) {
+		const worldBounds = this.worldSystem.bounds;
+		const parentBounds = parent.bounds;
 
-		if(parent.position[0] < ax
-			&& parent.velocity[0] < 0){
-			parent.position[0] = bx;
-			this.fire("wrap", parent);
+		const ax1 = worldBounds[0];
+		const ay1 = worldBounds[1];
+		const ax2 = worldBounds[2];
+		const ay2 = worldBounds[3];
+		const az1 = worldBounds[4];
+		const az2 = worldBounds[5];
+
+		const px = parent.position[0];
+		const py = parent.position[1];
+		const pz = parent.position[2];
+
+		const vx = parent.velocity[0];
+		const vy = parent.velocity[1];
+		const vz = parent.velocity[2];
+
+		const bx1 = parentBounds ? ax1 - parentBounds[2] : ax1;
+		const bx2 = parentBounds ? ax2 - parentBounds[0] : ax1;
+		const by1 = parentBounds ? ay1 - parentBounds[3] : ay1;
+		const by2 = parentBounds ? ay2 - parentBounds[1] : ay1;
+		const bz1 = parentBounds ? az1 - parentBounds[5] : az1;
+		const bz2 = parentBounds ? az2 - parentBounds[4] : az1;
+
+		if(px < bx1 && vx < 0){
+			parent.position[0] = bx2;
+			parent.fire("wrap", parent);	// [-1,0,0]
 		}
-		else if(parent.position[0] > bx
-			&& parent.velocity[0] > 0){
-			parent.position[0] = ax;
-			this.fire("wrap", parent);
+		else if(px > bx2 && vx > 0){
+			parent.position[0] = bx1;
+			parent.fire("wrap", parent);	// [1,0,0]
 		}
 
-		if(parent.position[1] < ay
-			&& parent.velocity[1] < 0){
-			parent.position[1] = by;
-			this.fire("wrap", parent);
+		if(py < by1 && vy < 0){
+			parent.position[1] = by2;
+			parent.fire("wrap", parent);	// [0,-1,0]
 		}
-		else if(parent.position[1] > by
-			&& parent.velocity[1] > 0){
-			parent.position[1] = ay;
-			this.fire("wrap", parent);
+		else if(py > by2 && vy > 0){
+			parent.position[1] = by1;
+			parent.fire("wrap", parent);	// [0,1,0]
 		}
 
-		if(parent.position[2] < az
-			&& parent.velocity[2] < 0){
-			parent.position[2] = bz;
-			this.fire("wrap", parent);
+		if(pz < bz1 && vz < 0){
+			parent.position[2] = bz2;
+			parent.fire("wrap", parent);	// [0,0,-1]
 		}
-		else if(parent.position[2] > bz
-			&& parent.velocity[2] > 0){
-			parent.position[2] = az;
-			this.fire("wrap", parent);
+		else if(pz > bz2 && vz > 0){
+			parent.position[2] = bz1;
+			parent.fire("wrap", parent);	// [0,0,1]
 		}
 	}
 }
